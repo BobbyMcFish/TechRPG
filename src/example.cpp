8e52ba368e86342f0518fd5164c9bcd738fb4731
BobbyMcFish <craigb119@hotmail.co.uk>
2017-12-21 19:20:44 +0000

    Creation
    Start of the project, empty oxygine engine compliles in VS2017

---
+++ src/example.cpp
@@ -0,0 +1,22 @@
+#include "oxygine-framework.h"
+#include <functional>
+using namespace oxygine;
+
+
+void example_preinit() {}
+
+//called from main.cpp
+void example_init()
+{
+}
+
+
+//called each frame from main.cpp
+void example_update()
+{
+}
+
+//called each frame from main.cpp
+void example_destroy()
+{
+}
